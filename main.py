c5d01a2ffbbb90e280d0449a5ccf9bb7659b3843
ahayeva <138230094+ahayeva@users.noreply.github.com>
2023-07-28 17:01:30 +0300

    zoo

--- main.py
+++ main.py
@@ -1,40 +1,82 @@
-def is_prime(number):
-    if number < 2:
-        return False
-    for i in range(2, int(number ** 0.5) + 1):
-        if number % i == 0:
-            return False
-    return True
+import json
+def add_to_json(data_animal):
+    try:
+        with open('animals.json', 'r') as file:
+            data = json.load(file)
+    except Exception as err:
+        data= []
+    data.append(data_animal)
+    with open('data.json', 'w') as json_file:
 
-def print_primes_in_range(start, end):
-    print(f"Прості числа в діапазоні від {start} до {end}:")
-    for number in range(start, end + 1):
-        if is_prime(number):
-            print(number, end=' ')
-    print()
+
+
+
 if __name__ == "__main__":
- start_range = int(input("Введіть початок діапазону "))
-end_range = int(input("Введіть кінець діапазону "))
+    nick = input("введіть ім'я тварини: ")
+    breed = input("введіть породу тварини: ")
+    food = input("підберіть їжу для тварини")
+    type = input("тип тварини")
+    animal_frame = {
+        "nick": "nick",
+        "breed": "breed",
+        "food": "food",
+        "type": "type"
+        add_to_json(data_animal)
+    }
+    add_to_json(data_animal)
 
-  print_primes_in_range(start_range, end_range)
-#EXERCISE 2
-def print_multiplication_table():
-    for i in range(1, 11):
-        for j in range(1, 11):
-            print(f"{i}*{j} = {i*j}", end=" ")
-        print()
 
-print(" множення від 1 до 10:")
-print_multiplication_table()
-#EXERCISE 3
-def print_custom_multiplication_table(start, end):
-    for i in range(start, end + 1):
-        for j in range(1, 11):
-            print(f"{i}*{j} = {i*j}", end=" ")
-        print()
 
-start_range = int(input(" початок діапазону: "))
-end_range = int(input(" кінець діапазону: "))
 
-print(f"Таблиця множення від {start_range} до {end_range}:")
-print_custom_multiplication_table(start_range, end_range)
+
+
+
+
+
+
+
+
+
+# def sumNumbers(a, b):
+#     return a + b
+#
+# def createIntials (name, last_name, fathers_name):
+#     intitials = False
+#     try:
+#         intitials = f"{last_name.capitalize()} {name[0].upper()}. {fathers_name[0].upper()}."
+#     except Exception as err:
+#         print("сталась помилка")
+#     return intitials
+#
+#
+# def runProgram():
+#     sumNumbers(1, 2)
+#     print(createIntials("Anna", "Ahaieva","Pavlivna"))
+#     if intitials:
+#         print(intitials)
+#     temperature = input("введіть температуру")
+#     if temperature >0:
+#         print("teplo")
+#
+
+#     active = True
+#     while active:
+#         try:
+#             runProgram()
+#         except Exception as err:
+#             print(err)
+#         finally:
+#             ch = input("бажаєте продовжити користування (N | Y):")
+#             if ch.lower() == "n":
+#                 active = False
+
+    # try:
+        # num =int("test")
+    #     print(num)
+    # except Exception as err: #передбачення помилки
+    #     try:
+    #         print(err)
+    #         print('')
+    #
+    # finally:
+    #     print("Some text")
